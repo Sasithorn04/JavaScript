9251434fc4a08a6cd7f5fbe16ade926f90ad0156
Sasithorn04 <72382051+Sasithorn04@users.noreply.github.com>
2021-01-15 15:42:33 +0700

    Delete Trip Planner.js

--- Trip Planner.js
+++
@@ -1,8 +0,0 @@
-/*
-คำนวณระยะเวลาการเดินทาง 40 ไมล์/ชั่วโมง
-input = ระยะทาง (ไมล์) output = ระยะเวลา (นาที) 
-*/
-function main() {
-    distance = prompt("ระยะทาง");
-    console.log(distance/40*60);
-}
